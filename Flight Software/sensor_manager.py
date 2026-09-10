6f95c246852c1f5d994ce60a2e75fe3af84e1261
Vatsal-parwani <vatsalparwani@gmail.com>
2026-09-10 22:28:30 +0100

    Can we merge both of the branches pls (#1) (nvm i did the merge)
    * Few changes to sensor and radio manager
    
    nothing major:
    - none for -999.0 so errors can be realised over the radio
    - changed freq to 433.0 for cansat legal + added variable for easy change
    - used lightweigth comma separated format for radio transmission for fast
    
    * Backend code
    
    Wrote most of backend code.

--- Flight Software/sensor_manager.py
+++ Flight Software/sensor_manager.py
@@ -3,8 +3,8 @@ import board
 import adafruit_bmp280
 
 i2c = busio.I2C(
-    scl = board.GP1,
-    sda = board.GP0
+    scl=board.GP1,
+    sda=board.GP0
 )
 
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
@@ -14,17 +14,19 @@ def get_readings():
         temperature = bmp280.temperature
         pressure = bmp280.pressure
     except RuntimeError:
+#print() for ground testing and impossible numbers for errors in acutal run
         print("Sensor communication error")
-        temperature = None
-        pressure = None
+        temperature = -999.0
+        pressure = -999.0
+#calculate pressure on ground        
     except Exception as problem:
         print(problem, "Error")
-        temperature = None
-        pressure = None
-    # Can add data validity later on after figuring out the bounds
+        temperature = -999.0
+        pressure = -999.0
+        
     data = {
         "temperature": temperature,
         "pressure": pressure
     }
-    
+        
     return data
